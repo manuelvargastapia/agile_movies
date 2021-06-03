27d13c272b4e6760dafd8cc4faa4d46f73de86bb
manuelvargastapia <manuelvargastapia@gmail.com>
2021-06-03 16:01:28 -0400

    add Actor to domain/movies

---
+++ src/domain/movies/actor.ts
@@ -0,0 +1,9 @@
+export class Actor {
+    name: string;
+    profileImageUrl: string;
+
+    constructor(name: string, profileImageUrl: string) {
+        this.name = name;
+        this.profileImageUrl = profileImageUrl;
+    }
+}
