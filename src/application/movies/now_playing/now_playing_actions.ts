a5a43e715b65e91f74376e0823b3e36f2185e5cd
manuelvargastapia <manuelvargastapia@gmail.com>
2021-06-03 16:02:35 -0400

    refactor to use IMovie common interface

--- src/application/movies/now_playing/now_playing_actions.ts
+++ src/application/movies/now_playing/now_playing_actions.ts
@@ -1,14 +1,18 @@
 import { Dispatch } from 'redux';
 import { Token } from '../../../domain/core/value_objects';
-import { getNowPlayingMovies } from '../../../infrastructure/movies/movies_repository';
+import {
+    getMovies,
+    MovieAPI,
+} from '../../../infrastructure/movies/movies_repository';
 import { nowPlayingActions } from './now_playing_slice';
 
 export const fetchNowPlayingMovies = (token: string, pageNumber: number) => {
     return async (dispatch: Dispatch) => {
         dispatch(nowPlayingActions.fetchingStarted());
-        const movieFailureOrData = await getNowPlayingMovies(
+        const movieFailureOrData = await getMovies(
             new Token(token),
             pageNumber,
+            MovieAPI.NOW_PLAYING,
         );
         dispatch(nowPlayingActions.getNowPlayingMovies(movieFailureOrData));
     };
--- src/application/movies/popular/popular_actions.ts
+++ src/application/movies/popular/popular_actions.ts
@@ -1,14 +1,18 @@
 import { Dispatch } from 'redux';
 import { Token } from '../../../domain/core/value_objects';
-import { getPopularMovies } from '../../../infrastructure/movies/movies_repository';
+import {
+    getMovies,
+    MovieAPI,
+} from '../../../infrastructure/movies/movies_repository';
 import { popularActions } from './popular_slice';
 
 export const fetchPopularMovies = (token: string, pageNumber: number) => {
     return async (dispatch: Dispatch) => {
         dispatch(popularActions.fetchingStarted());
-        const movieFailureOrData = await getPopularMovies(
+        const movieFailureOrData = await getMovies(
             new Token(token),
             pageNumber,
+            MovieAPI.POPULAR,
         );
         dispatch(popularActions.getPopularMovies(movieFailureOrData));
     };
