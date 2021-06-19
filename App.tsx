9ec81026352d4a921727bae46ee412556cbf8fc9
manuelvargastapia <manuelvargastapia@gmail.com>
2021-06-19 18:43:29 -0400

    provide theme from rn paper

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
 import { appStore } from './src/application/store';
-import SplashPage from './src/presentation/splash/SplashPage';
+import Router from './src/presentation/router/Router';
 
 const App = () => {
     return (
         <Provider store={appStore}>
-            <SplashPage />
+            <PaperProvider
+                theme={{
+                    ...DefaultTheme,
+                    dark: true,
+                }}>
+                <Router />
+            </PaperProvider>
         </Provider>
     );
 };
