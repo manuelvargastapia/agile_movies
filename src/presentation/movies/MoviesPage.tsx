9bae68b9c1b2e6dc005eebb8fdc0e4ee8fd43f0e
manuelvargastapia <manuelvargastapia@gmail.com>
2021-06-18 21:18:04 -0400

    make MoviesPage handle initial login and potential redirecting to /login

--- src/presentation/movies/MoviesPage.tsx
+++ src/presentation/movies/MoviesPage.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { useHistory } from 'react-router-native';
+import { loginWithStoredToken } from '../../application/authentication/login/login_actions';
+import { useAppDispatch, useAppSelector } from '../../application/hooks';
+import { AuthData } from '../../domain/authentication/auth_data';
 import NowPlayingList from './components/NowPlayingList';
 
 const MoviesPage = () => {
+    const { isLoggedIn, authFailureOrData } = useAppSelector(
+        ({ login }) => login,
+    );
+
+    const dispatch = useAppDispatch();
+
+    const history = useHistory();
+
+    useEffect(() => {
+        dispatch(loginWithStoredToken());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            history.replace('/login');
+        }
+    }, [history, isLoggedIn]);
+
     return (
         <View>
             <Text>MOVIES</Text>
-            <NowPlayingList />
+            {isLoggedIn && authFailureOrData instanceof AuthData && (
+                <NowPlayingList authData={authFailureOrData} />
+            )}
         </View>
     );
 };
