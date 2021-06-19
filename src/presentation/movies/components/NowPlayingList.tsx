986d4b8c1a6d96d7f0d98bee89251169e936601e
manuelvargastapia <manuelvargastapia@gmail.com>
2021-06-18 21:18:51 -0400

    simiplify UI to only care about token expiration and data fetching

--- src/presentation/movies/components/NowPlayingList.tsx
+++ src/presentation/movies/components/NowPlayingList.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
-import { useHistory } from 'react-router-native';
 import Carousel from 'react-native-snap-carousel';
 import { Circle } from 'react-native-progress';
 import { AuthData } from '../../../domain/authentication/auth_data';
@@ -8,31 +7,33 @@ import { fetchNowPlayingMovies } from '../../../application/movies/now_playing/n
 import { MovieFailure } from '../../../domain/movies/movie_failures';
 import { NowPlayingMovie } from '../../../domain/movies/now_playing';
 import { useAppDispatch, useAppSelector } from '../../../application/hooks';
+import { loginWithRefreshToken } from '../../../application/authentication/login/login_actions';
+import { nowPlayingActions } from '../../../application/movies/now_playing/now_playing_slice';
 
-const NowPlayingList = () => {
+const NowPlayingList: React.FC<{ authData: AuthData }> = ({ authData }) => {
     const { isFetching, pageNumber, movieFailureOrData, tokenExpired } =
         useAppSelector(({ nowPlaying }) => nowPlaying);
-    const { isLoggedIn, authFailureOrData } = useAppSelector(
-        ({ login }) => login,
-    );
-    const movieDispatch = useAppDispatch();
-    const history = useHistory();
+
+    const dispatch = useAppDispatch();
 
     useEffect(() => {
-        if (authFailureOrData instanceof AuthData) {
-            movieDispatch(
-                fetchNowPlayingMovies(authFailureOrData.token.value, 1),
-            );
+        if (tokenExpired) {
+            dispatch(loginWithRefreshToken(authData.refreshToken));
         }
-    }, [authFailureOrData, isLoggedIn, movieDispatch]);
+    }, [authData.refreshToken, dispatch, tokenExpired]);
+
+    useEffect(() => {
+        dispatch(fetchNowPlayingMovies(authData.token, pageNumber));
+    }, [authData.token, dispatch, pageNumber]);
 
     return (
         <>
-            {isFetching && <Circle size={50} indeterminate />}
             {movieFailureOrData instanceof MovieFailure ? (
-                <View />
+                <View>
+                    <Text>{movieFailureOrData.message}</Text>
+                </View>
             ) : (
-                (movieFailureOrData as NowPlayingMovie[]).length > 0 && (
+                movieFailureOrData.length > 0 && (
                     <Carousel
                         data={movieFailureOrData}
                         renderItem={({ item }: { item: NowPlayingMovie }) => (
@@ -42,6 +43,16 @@ const NowPlayingList = () => {
                         )}
                         sliderWidth={500}
                         itemWidth={200}
+                        onEndReached={() => {
+                            dispatch(nowPlayingActions.incrementPageNumber());
+                        }}
+                        ListFooterComponent={
+                            <>
+                                {isFetching && (
+                                    <Circle size={50} indeterminate />
+                                )}
+                            </>
+                        }
                     />
                 )
             )}
