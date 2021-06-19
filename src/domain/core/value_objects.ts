b45faffa73e7622c1fdf2b1c2d5d3596d70146a9
manuelvargastapia <manuelvargastapia@gmail.com>
2021-06-18 21:13:24 -0400

    add isValid and getOrCrash helper methods for all the value objects to handle potential failures directly

--- src/domain/core/value_objects.ts
+++ src/domain/core/value_objects.ts
@@ -1,101 +1,141 @@
+import {
+    MissingRefreshToken,
+    MissingToken,
+    ValueFailure,
+} from './value_failures';
+
 abstract class ValueObject<T> {
-    value: T;
+    value: ValueFailure<T> | T;
 
-    constructor(value: T) {
+    constructor(value: ValueFailure<T> | T) {
         this.value = value;
+    }
+
+    isValid(): boolean {
+        return !(this.value instanceof ValueFailure);
+    }
+
+    getOrCrash(): T {
+        if (this.value instanceof ValueFailure) {
+            throw new Error('Unexpected error!');
+        } else {
+            return this.value;
+        }
     }
 }
 
-export class EmailAddress implements ValueObject<string> {
+export class EmailAddress extends ValueObject<string> {
     value: string;
 
     constructor(value: string) {
+        super(value);
         this.value = value;
     }
 }
 
-export class Username implements ValueObject<string> {
+export class Username extends ValueObject<string> {
     value: string;
 
     constructor(value: string) {
+        super(value);
         this.value = value;
     }
 }
 
-export class Password implements ValueObject<string> {
+export class Password extends ValueObject<string> {
     value: string;
 
     constructor(value: string) {
+        super(value);
         this.value = value;
     }
 }
 
-export class Firstname implements ValueObject<string> {
+export class Firstname extends ValueObject<string> {
     value: string;
 
     constructor(value: string) {
+        super(value);
         this.value = value;
     }
 }
 
-export class Lastname implements ValueObject<string> {
+export class Lastname extends ValueObject<string> {
     value: string;
 
     constructor(value: string) {
+        super(value);
         this.value = value;
     }
 }
 
-export class Token implements ValueObject<string> {
+export class Token extends ValueObject<string> {
+    value: string | ValueFailure<string>;
+
+    constructor(value: null | string) {
+        if (typeof value === 'string') {
+            super(value);
+            this.value = value;
+            return;
+        }
+        super(new MissingToken());
+        this.value = new MissingToken();
+    }
+}
+
+export class RefreshToken extends ValueObject<string> {
+    value: string | ValueFailure<string>;
+
+    constructor(value: null | string) {
+        if (typeof value === 'string') {
+            super(value);
+            this.value = value;
+            return;
+        }
+        super(new MissingRefreshToken());
+        this.value = new MissingRefreshToken();
+    }
+}
+
+export class MovieBannerUrl extends ValueObject<string> {
     value: string;
 
     constructor(value: string) {
+        super(value);
         this.value = value;
     }
 }
 
-export class RefreshToken implements ValueObject<string> {
+export class MovieCoverUrl extends ValueObject<string> {
     value: string;
 
     constructor(value: string) {
+        super(value);
         this.value = value;
     }
 }
 
-export class MovieBannerUrl implements ValueObject<string> {
-    value: string;
-
-    constructor(value: string) {
-        this.value = value;
-    }
-}
-
-export class MovieCoverUrl implements ValueObject<string> {
-    value: string;
-
-    constructor(value: string) {
-        this.value = value;
-    }
-}
-
-export class MovieId implements ValueObject<number> {
+export class MovieId extends ValueObject<number> {
     value: number;
 
     constructor(value: number) {
+        super(value);
         this.value = value;
     }
 }
-export class MovieTitle implements ValueObject<string> {
+export class MovieTitle extends ValueObject<string> {
     value: string;
 
     constructor(value: string) {
+        super(value);
         this.value = value;
     }
 }
-export class MovieOverview implements ValueObject<string> {
+export class MovieOverview extends ValueObject<string> {
     value: string;
 
     constructor(value: string) {
+        super(value);
         this.value = value;
     }
 }
