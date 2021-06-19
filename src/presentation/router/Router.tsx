1d64516a5cc2775ae7a66c9c5393575d57ee50f1
manuelvargastapia <manuelvargastapia@gmail.com>
2021-06-19 18:44:49 -0400

    handle initial auth logic directly in router to fix issue regarding splash page's unintended behaviour

--- src/presentation/router/Router.tsx
+++ src/presentation/router/Router.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { NativeRouter, Redirect, Route } from 'react-router-native';
-import { useAppSelector } from '../../application/hooks';
+import { loginWithStoredToken } from '../../application/authentication/login/login_actions';
+import { useAppDispatch, useAppSelector } from '../../application/hooks';
 import LoginPage from '../authentication/LoginPage';
 import MoviesPage from '../movies/MoviesPage';
 
 const Router = () => {
-    const { isLoggedIn } = useAppSelector(({ login }) => login);
+    // This is needed because isSubmitting is initially false, so we
+    // needa way of skipping the first render when evaluating it
+    const firstRender = useRef(true);
+
+    const { isLoggedIn, isSubmitting } = useAppSelector(({ login }) => login);
+
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        firstRender.current = false;
+        dispatch(loginWithStoredToken());
+    }, [dispatch]);
 
     return (
         <NativeRouter>
-            <Route exact path="/">
-                {isLoggedIn ? (
-                    <Redirect to="/movies" />
-                ) : (
-                    <Redirect to="/login" />
-                )}
-            </Route>
+            {!firstRender.current && !isSubmitting && (
+                <Route exact path="/">
+                    {isLoggedIn ? (
+                        <Redirect to="/movies" />
+                    ) : (
+                        <Redirect to="/login" />
+                    )}
+                </Route>
+            )}
             <Route path="/movies" component={MoviesPage} />
             <Route path="/login" component={LoginPage} />
         </NativeRouter>
