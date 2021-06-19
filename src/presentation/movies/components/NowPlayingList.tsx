23faf6f0e2afc3cabfb27b731301f9584d44111f
manuelvargastapia <manuelvargastapia@gmail.com>
2021-06-19 15:36:04 -0400

    add images and improve UI in general

--- src/presentation/movies/components/NowPlayingList.tsx
+++ src/presentation/movies/components/NowPlayingList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import { Circle } from 'react-native-progress';
 import { AuthData } from '../../../domain/authentication/auth_data';
@@ -38,21 +38,30 @@ const NowPlayingList: React.FC<{ authData: AuthData }> = ({ authData }) => {
                         data={movieFailureOrData}
                         renderItem={({ item }: { item: NowPlayingMovie }) => (
                             <View>
-                                <Text>{item.movieTitle.value}</Text>
+                                <Image
+                                    style={styles.bannerImage}
+                                    resizeMode="contain"
+                                    source={{
+                                        uri: item.movieBannerUrl.value,
+                                    }}
+                                />
                             </View>
                         )}
-                        sliderWidth={500}
-                        itemWidth={200}
+                        sliderWidth={Dimensions.get('screen').width}
+                        itemWidth={Dimensions.get('screen').width * 0.5}
                         onEndReached={() => {
                             dispatch(nowPlayingActions.incrementPageNumber());
                         }}
                         ListFooterComponent={
                             <>
                                 {isFetching && (
-                                    <Circle size={50} indeterminate />
+                                    <View style={styles.loaderContainer}>
+                                        <Circle size={50} indeterminate />
+                                    </View>
                                 )}
                             </>
                         }
+                        keyExtractor={(_, index) => index.toString()}
                     />
                 )
             )}
@@ -61,3 +70,15 @@ const NowPlayingList: React.FC<{ authData: AuthData }> = ({ authData }) => {
 };
 
 export default NowPlayingList;
+
+const styles = StyleSheet.create({
+    bannerImage: {
+        height: Dimensions.get('screen').width * 0.6,
+    },
+    loaderContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 50,
+    },
+});
