8be94d9d912a0edda5167bd9755383b499d6e9fa
manuelvargastapia <manuelvargastapia@gmail.com>
2021-06-18 21:15:34 -0400

    create async storage repository to manage tokens

---
+++ src/infrastructure/authentication/async_storage_repository.ts
@@ -0,0 +1,47 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AuthData } from '../../domain/authentication/auth_data';
+import {
+    AuthFailure,
+    NoDataStored,
+    StorageFailed,
+} from '../../domain/authentication/auth_failures';
+import { RefreshToken, Token } from '../../domain/core/value_objects';
+
+export async function storeAuthData(
+    authData: AuthData,
+): Promise<AuthFailure | void> {
+    try {
+        if (authData.token.isValid()) {
+            await AsyncStorage.setItem('@token', authData.token.getOrCrash());
+        }
+
+        if (authData.refreshToken.isValid()) {
+            await AsyncStorage.setItem(
+                '@refresh_token',
+                authData.refreshToken.getOrCrash(),
+            );
+        }
+    } catch (error) {
+        return new StorageFailed('Error storing authentication tokens');
+    }
+}
+
+export async function readAuthData(): Promise<AuthFailure | AuthData> {
+    try {
+        const token = await AsyncStorage.getItem('@token');
+        const refreshToken = await AsyncStorage.getItem('@refresh_token');
+
+        if (token === null) {
+            return new NoDataStored('There are no tokens');
+        }
+
+        const authData = new AuthData(
+            new Token(token),
+            new RefreshToken(refreshToken),
+        );
+
+        return authData;
+    } catch (error) {
+        return new StorageFailed('Error retrieving autthentication tokens');
+    }
+}
